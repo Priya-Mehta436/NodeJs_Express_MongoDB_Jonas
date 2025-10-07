f77d0454904d6f8b8a4bb9fdb68dc47ca4fc6608
Priya Mehta <pmehta12122005@gmail.com>
2025-10-07 13:28:35 +0530

    Building Simple API

--- 1-Node-Farm/index.js
+++ 1-Node-Farm/index.js
@@ -31,6 +31,11 @@ const url = require('url');
 
 /////////////////////////////////////////////////
 //SERVER
+
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req,res) => {
     const pathName = req.url;
 
@@ -39,6 +44,10 @@ const server = http.createServer((req,res) => {
     }
     else if (pathName === '/product'){
         res.end('This is the PRODUCT');
+    }
+     else if (pathName === '/api'){
+            res.writeHead(200, {'Content-type': 'application/json'});
+            res.end(data);
     }
     else {
         res.writeHead(404,{
