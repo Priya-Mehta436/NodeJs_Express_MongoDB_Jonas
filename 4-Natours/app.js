db0be26c50d5199e1162b332a1ed49e088bb84d9
Priya Mehta <pmehta12122005@gmail.com>
2025-10-09 15:26:15 +0530

    Handling POST Requests

--- 4-Natours/app.js
+++ 4-Natours/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req,res) => {
 //  res
 //  .status(404)
@@ -12,7 +14,7 @@ const app = express();
 // })
 
 // app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...');
+//   res.send('You  can post to this endpoint...');
 // });
 
 const tours = JSON.parse(
@@ -32,7 +34,24 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  res.send('You can post to this endpoint...');
+    // console.log(req.body);
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours, err => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    tour: newTour
+                }
+            });
+        })
+    );
+
 });
 
 const port = 3000;
