935ff10c30e9105233e4e4e552dcca0bfdd294af
Priya Mehta <pmehta12122005@gmail.com>
2025-10-10 13:32:26 +0530

    Chaining Multiple Middleware

--- 4-Natours/routes/tourRoutes.js
+++ 4-Natours/routes/tourRoutes.js
@@ -4,9 +4,17 @@ const tourController = require('../controllers/tourController');
 const Router = express.Router();
 
 Router.param('id',tourController.checkID);
+
+// create a checkBody middleware
+// check if the body contains the name and price property
+// if not, send back 400 (bad request)
+// add it to the post handler stack
+
+
+
 Router.route('/')
    .get(tourController.getAllTours)
-   .post(tourController.createTour);
+   .post(tourController.checkBody, tourController.createTour);
    
 Router.route('/:id')
    .get(tourController.getTour)
