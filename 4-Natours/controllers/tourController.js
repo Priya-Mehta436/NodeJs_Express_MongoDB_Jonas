4d9c14aadd41e0ac5a36aa3534cb9a0dc01de3c8
Priya Mehta <pmehta12122005@gmail.com>
2025-10-10 13:24:18 +0530

    Param Middleware

--- 4-Natours/controllers/tourController.js
+++ 4-Natours/controllers/tourController.js
@@ -4,9 +4,22 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+     console.log(`Tour id is: ${val}`);
+     if(req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+next();
+};
+
 exports.getAllTours = (req, res) => {
+    console.log(req.requestTime);
     res.status(200).json({
         status: 'success', 
+        requestedAt: req.requestTime,
         results: tours.length,
         data: { 
             tours
@@ -19,15 +32,13 @@ exports.getTour = (req, res) => {
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
 
-    //if (id >= tours.length) {
-    if(!tour) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
+    res.status(200).json({
+        status: 'success',
+        data: { 
+            tour
+        },
+    });
 }
-
 exports.createTour = (req, res) => {
     //console.log(req.body);
     const newId = tours[tours.length - 1].id + 1;
@@ -44,12 +55,6 @@ exports.createTour = (req, res) => {
 }
 
 exports.updateTour = (req, res) => {
-    if(req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
     res.status(200).json({
         status: 'success',
         data: {
@@ -59,12 +64,7 @@ exports.updateTour = (req, res) => {
 }
 
 exports.deleteTour = (req, res) => {  
-    if(req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
+   
     res.status(204).json({
         status: 'success',
         data: null
