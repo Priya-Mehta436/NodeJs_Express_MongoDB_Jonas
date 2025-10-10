5b97c903344a44352ab9259c704c3048f77aeef2
Priya Mehta <pmehta12122005@gmail.com>
2025-10-10 14:05:46 +0530

    Environment Variables

--- 4-Natours/app.js
+++ 4-Natours/app.js
@@ -4,7 +4,10 @@ const morgan = require('morgan');
 const app = express();
 
 //1)Middlewares
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
